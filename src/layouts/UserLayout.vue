f4cb13b278bd21936f910a14b480cbcb1a6401f5
matao <matao@wjmt.me>
2019-08-02 16:07:17 +0800

    user

--- src/layouts/UserLayout.vue
+++ src/layouts/UserLayout.vue
@@ -5,24 +5,25 @@
         <div class="header">
           <a href="/">
             <img src="~@/assets/logo.svg" class="logo" alt="logo">
-            <span class="title">Ant Design</span>
+            <span class="title">ALPHA</span>
           </a>
         </div>
         <div class="desc">
-          Ant Design 是西湖区最具影响力的 Web 设计规范
+          ALPHA
         </div>
       </div>
 
       <route-view></route-view>
 
       <div class="footer">
-        <div class="links">
-          <a href="_self">帮助</a>
-          <a href="_self">隐私</a>
-          <a href="_self">条款</a>
-        </div>
+<!--        <div class="links">-->
+<!--          <a href="_self">帮助</a>-->
+<!--          <a href="_self">隐私</a>-->
+<!--          <a href="_self">条款</a>-->
+<!--        </div>-->
         <div class="copyright">
-          Copyright &copy; 2018 白鹭学园技术组出品
+<!--          Copyright &copy; 2018 白鹭学园技术组出品-->
+          MT
         </div>
       </div>
     </div>
