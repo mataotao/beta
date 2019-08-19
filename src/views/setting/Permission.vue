c66975ccebb34c245235bdabd862dbb443567429
matao <matao@wjmt.me>
2019-08-19 16:20:52 +0800

    permission

--- src/views/setting/Permission.vue
+++ src/views/setting/Permission.vue
@@ -2,41 +2,44 @@
   <a-card :bordered="false">
     <a-row :gutter="8">
       <a-col :offset="4" :span="8">
-        <s-tree
-          :dataSource="orgTree"
-          @click="handleClick"
-          @add="handleAdd"
-          @titleClick="handleTitleClick">
-
-        </s-tree>
+        <a-tree
+          :treeData="treeData"
+          defaultExpandAll
+          @rightClick="setting"
+        >
+        </a-tree>
       </a-col>
     </a-row>
 
-    <org-modal ref="modal" @ok="handleSaveOk" @close="handleSaveClose" />
   </a-card>
 </template>
-
 <script>
-import STree from '@/components/Tree/Tree'
-import { STable } from '@/components'
-import OrgModal from './modules/OrgModal'
 import { getOrgTree } from '@/api/manage'
 
 export default {
-  components: {
-    STable,
-    STree,
-    OrgModal
-  },
   data () {
     return {
-      orgTree: []
+      treeData: [],
+      parentData: {
+        id: 0,
+        label: '菜单',
+        pid: 0,
+        level: 0,
+        url: '',
+        sort: 0,
+        component: '',
+        icon: 0,
+        children: []
+      },
+      all: true
     }
   },
   created () {
     getOrgTree().then(res => {
-      this.orgTree = res.data
-      this.conversionTree(res.data)
+      this.parentData.children = res.data
+      const menu = [this.parentData]
+      this.treeData = menu
+      this.conversionTree(menu)
     })
   },
   methods: {
@@ -53,71 +56,15 @@ export default {
         }
       })
     },
-    handleClick (e) {
-      console.log('handleClick', e)
-      this.queryParam = {
-        key: e.key
-      }
+    onSelect (selectedKeys, info) {
+      console.log('selected', selectedKeys, info)
     },
-    handleAdd (item) {
-      console.log('add button, item', item)
-      this.$refs.modal.add(item.title)
+    onCheck (checkedKeys, info) {
+      console.log('onCheck', checkedKeys, info)
     },
-    handleTitleClick (item) {
-      console.log('handleTitleClick', item)
-    },
-    titleClick (e) {
-      console.log('titleClick', e)
-    },
-    handleSaveOk () {
-
-    },
-    handleSaveClose () {
-
-    },
-
-    onSelectChange (selectedRowKeys, selectedRows) {
-      this.selectedRowKeys = selectedRowKeys
-      this.selectedRows = selectedRows
+    setting ({ event, node }) {
+      console.log(node.$options.propsData.dataRef)
     }
   }
 }
 </script>
-
-<style lang="less">
-  .custom-tree {
-
-    /deep/ .ant-menu-item-group-title {
-      position: relative;
-      &:hover {
-        .btn {
-          display: block;
-        }
-      }
-    }
-
-    /deep/ .ant-menu-item {
-      &:hover {
-        .btn {
-          display: block;
-        }
-      }
-    }
-
-    /deep/ .btn {
-      display: none;
-      position: absolute;
-      top: 0;
-      right: 10px;
-      width: 20px;
-      height: 40px;
-      line-height: 40px;
-      z-index: 1050;
-
-      &:hover {
-        transform: scale(1.2);
-        transition: 0.5s all;
-      }
-    }
-  }
-</style>
